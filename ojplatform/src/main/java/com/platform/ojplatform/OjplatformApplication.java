10ea0d1ed40702fb6bdd18b1aad6dc09e93badc1
HongyiHao-SXIT <Lanyi_adict@outlook.com>
2025-01-10 23:06:16 +0800

    Maven projects

---
+++ ojplatform/src/main/java/com/platform/ojplatform/OjplatformApplication.java
@@ -0,0 +1,13 @@
+package com.platform.ojplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OjplatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OjplatformApplication.class, args);
+	}
+
+}
--- ojplatform/src/test/java/com/platform/ojplatform/OjplatformApplicationTests.java
+++ ojplatform/src/test/java/com/platform/ojplatform/OjplatformApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.platform.ojplatform;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class OjplatformApplicationTests {
 
 	@Test
 	void contextLoads() {
