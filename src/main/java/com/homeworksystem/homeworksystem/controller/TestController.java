538ffdad9c782251a4af67a64455b1c4765447c6
HongyiHao-SXIT <Lanyi_adict@outlook.com>
2025-03-29 22:12:20 +0800

    add function page

--- src/main/java/com/homeworksystem/homeworksystem/controller/TestController.java
+++ src/main/java/com/homeworksystem/homeworksystem/controller/TestController.java
@@ -10,6 +10,7 @@ public class TestController{
     @RequestMapping("/hello")
     public String getPage(HashMap<String, String> map){
         map.put("title", "欢迎来到Spring Boot!");
+        map.put("key", "Do you want try this website");
         return "/index";
     }
 }
---
+++ src/main/java/com/homeworksystem/homeworksystem/model/User.java
@@ -0,0 +1,15 @@
+package ;
+
+public class abstract User{
+    private Long id;
+
+    private String Account;
+
+    private String password;
+
+    private String name;
+
+    
+
+
+}
