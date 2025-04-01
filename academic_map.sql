57dcdcb4dcc15753caba7c4a9f91efffb9f2bcfd
HongyiHao-SXIT <Lanyi_adict@outlook.com>
2025-04-01 23:50:42 +0800

    fix the git

--- academic_map.sql
+++ academic_map.sql
@@ -2,9 +2,4 @@
 -- Active: 1736786939860@@127.0.0.1@3306@academic
 CREATE DATABASE academic;
 
-=======
--- Active: 1736786939860@@127.0.0.1@3306@academic
-CREATE DATABASE academic;
-
->>>>>>> 2b2c06fa920178d49435315acd4cfc1353af164b
 CREATE Table User
--- src/main/java/com/hongming_academic_map/homgming_academic_map/controller/TestController.java
+++ src/main/java/com/hongming_academic_map/homgming_academic_map/controller/TestController.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.hongming_academic_map.homgming_academic_map.controller;
 
 import java.util.HashMap;
@@ -13,20 +12,5 @@ public class TestController{
         map.put("title", "欢迎来到Spring Boot!");
         return "/index";
     }
-=======
-package com.hongming_academic_map.homgming_academic_map.controller;
 
-import java.util.HashMap;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.stereotype.Controller;
- 
-@Controller
-public class TestController{
- 
-    @RequestMapping("/hello")
-    public String getPage(HashMap<String, String> map){
-        map.put("title", "欢迎来到Spring Boot!");
-        return "/index";
-    }
->>>>>>> 2b2c06fa920178d49435315acd4cfc1353af164b
 }
